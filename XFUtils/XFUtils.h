1739ea72bd51fcc0816c824d9582fd8c77e756b8
Manu Wallner <manu.wallner@me.com>
2014-09-16 09:21:08 +0200

    Initial Commit

---
+++ XFUtils/XFUtils.h
@@ -0,0 +1,13 @@
+//
+//  XFUtils.h
+//  XFUtils
+//
+//  Created by Manu Wallner on 16/09/14.
+//  Copyright (c) 2014 XForge Software Development GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XFUtils : NSObject
+
+@end
---
+++ XFUtils/XFUtils.m
@@ -0,0 +1,13 @@
+//
+//  XFUtils.m
+//  XFUtils
+//
+//  Created by Manu Wallner on 16/09/14.
+//  Copyright (c) 2014 XForge Software Development GmbH. All rights reserved.
+//
+
+#import "XFUtils.h"
+
+@implementation XFUtils
+
+@end
